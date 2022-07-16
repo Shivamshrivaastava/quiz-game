c1b63d187356490bb9b90278f772a3544fe084e5
Shivamshrivaastava <shivamshrivaastava@gmail.com>
2022-07-16 16:58:30 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,69 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "  Javascript is an which type of language?",
+    answer: "Object Oriented",
+    options: [
+      "Object Oriented",
+      "Object based",
+      "procedural",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question:" Among the given statements, which statement defines closures in JavaScript?",
+    answer: "JavaScript is a function that is enclosed with references to its lexical environment",
+    options: [
+    "  JavaScript is a function that is enclosed with references to its inner function scope",
+       "JavaScript is a function that is enclosed with references to its lexical environment",
+      " JavaScript is a function that is enclosed with the object to its inner function scope",
+       "None of the mentioned"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },  
+    {
+    numb: 6,
+    question: "Which of the following is not javascript data types?",
+    answer: "All of the mentioned",
+    options: [
+     " Null type",
+   " Undefined type",
+     " Number type",
+     "All of the mentioned"
+    ]
+  },
+];
